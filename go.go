0d2b29a73e305a7494d779a6420ba08714f685a3
BrainWen <2229553982@qq.com>
2025-11-26 23:42:31 +0800

    11.26 安装配置go环境，编写第一个go源文件并且运行成功

---
+++ go.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+
+	var x int = 42
+	fmt.Println("The value of x is:", x)
+
+	name := "Brian"
+	fmt.Println("Hello,", name)
+}
