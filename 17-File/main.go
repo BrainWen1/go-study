41a56b2ec2494e4935584530755a2a93b838a9e1
BrainWen <2229553982@qq.com>
2026-04-15 14:51:31 +0800

    4.15 文件操作：写 目录

--- 17-File/main.go
+++ 17-File/main.go
@@ -62,4 +62,72 @@ func main() {
 	for buf2.Scan() {           // 读取每个单词并且判断是否读取成功
 		fmt.Println(buf2.Text()) // 输出当前单词
 	}
+
+	// 一次性写
+	err = os.WriteFile("write.txt", []byte("Writing some text..."), 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	// 流式写
+	file, err = os.OpenFile("write.txt", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+	_, err = writer.WriteString(" Adding more text...\n")
+	if err != nil {
+		panic(err)
+	}
+	writer.Flush()
+
+	// 文件信息
+	info, err := os.Stat("write.txt")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("File Name: %s\nSize: %d bytes\nPermissions: %s\n",
+		info.Name(), info.Size(), info.Mode())
+
+	// 重命名、删除文件
+	file, err = os.Create("temp.txt")
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("temp.txt created !")
+	}
+	file.Close()
+
+	err = os.Rename("temp.txt", "renamed.txt")
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("temp.txt renamed to renamed.txt !")
+	}
+	err = os.Remove("renamed.txt")
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("renamed.txt removed !")
+	}
+
+	// 文件复制
+	src, _ := os.Open("hello.txt")
+	defer src.Close()
+	dst, _ := os.Create("copy.txt")
+	defer dst.Close()
+
+	_, err = io.Copy(dst, src)
+	if err != nil {
+		panic(err)
+	}
+
+	// 目录操作
+	dir, _ := os.ReadDir("..") // 读取上级目录"go-study"
+	for _, entry := range dir {
+		info, _ := entry.Info()
+		fmt.Println(entry.Name(), info.Size(), entry.IsDir())
+	}
 }
